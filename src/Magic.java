69f06760758bac5f83cb804b44203f1be035b3a7
chenqingspring <chenqing1213@gmail.com>
2012-02-10 16:22:23 +0800

    9th commit for change package

---
+++ src/Magic.java
@@ -0,0 +1,10 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: spring
+ * Date: 2012-2-1
+ * Time: 21:55:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Magic {
+}
---
+++ src/Tool.java
@@ -0,0 +1,11 @@
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: spring
+ * Date: 2012-2-1
+ * Time: 21:53:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tool {
+}
