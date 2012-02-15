7f91258646bed64047af87e554e1dbfdcd85d95e
chenqingspring <chenqing1213@gmail.com>
2012-02-15 20:44:06 +0800

    commit for color output

---
+++ src/ColorOutput.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: spring
+ * Date: 2012-2-15
+ * Time: 18:20:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class ColorOutput {
+}
